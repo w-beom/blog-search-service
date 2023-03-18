2e66000b220a1fc3cbf01e4a2cef4d9d2668175d
w-beom <w_bum@naver.com>
2023-03-18 15:10:04 +0900

    Initial commit

---
+++ src/main/java/com/blogsearchservice/BlogSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blogsearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSearchServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blogsearchservice/BlogSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogsearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
